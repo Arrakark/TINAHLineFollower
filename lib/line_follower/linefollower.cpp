039421c7a470023a8fca755f67f7ca1fdafe5b02
Vladislav Pomogaev <comsa44@gmail.com>
2018-05-25 22:50:11 -0700

    Added setup procedure. Untested

--- lib/line_follower/linefollower.cpp
+++ lib/line_follower/linefollower.cpp
@@ -1,7 +1,6 @@
 #include "linefollower.h"
 
-
-linefollower::linefollower(LiquidCrystal * passed_crystal, motorClass * passed_motor)
+linefollower::linefollower(LiquidCrystal *passed_crystal, motorClass *passed_motor)
 {
 	LCD = passed_crystal;
 	motor = passed_motor;
@@ -9,13 +8,30 @@ linefollower::linefollower(LiquidCrystal * passed_crystal, motorClass * passed_m
 
 void linefollower::setup()
 {
+	write_to_LCD("LINE FOLLOWER", "SETUP");
+	delay(1000);
+	write_to_LCD("USE KNOB 6", "TO ADJUST TRIM");
+	delay(1000);
+	while (!digitalRead(TRIM_KNOB))
+	{
+		write_to_LCD("CURRENT TRIM", String(trim));
+		follow_line();
+		set_trim();
+	}
+	write_to_LCD("ADJUST LEFT", "SENSOR");
+	for (int i = 40; i < 42; i++)
+	{
+		write_to_LCD(String("LSENS: ") + String(), String("SETPNT:") + String());
+	}
+}
+
+void linefollower::write_to_LCD(String line1, String line2)
+{
 	LCD->clear();
 	LCD->home();
-	LCD->setCursor(0, 0);
-	LCD->write("Line Follower");
+	LCD->write(line1);
 	LCD->setCursor(0, 1);
-	LCD->write("Setup!");
-  	delay(1000);
+	LCD->write(line2);
 }
 
 void linefollower::follow_line()
--- lib/line_follower/linefollower.h
+++ lib/line_follower/linefollower.h
@@ -3,25 +3,25 @@
 #include <LiquidCrystal.h>
 #include <motor.h>
 
+#define TRIM_KNOB PF6
 
 class linefollower
 {
-	public:
-		//consturctor
-		linefollower(LiquidCrystal*, motorClass*);
+  public:
+	//consturctor
+	linefollower(LiquidCrystal *, motorClass *);
 
-		//member functions
-		void setup();
-		//void set_speed(int speed);
-		void follow_line();
-		//void stop_following();
-		void set_trim();
-	
-	private:
-		motorClass * motor;
-		LiquidCrystal * LCD;
-		int trim;
+	//member functions
+	void setup();
+	//void set_speed(int speed);
+	void follow_line();
+	//void stop_following();
+	void set_trim();
+
+	void write_to_LCD(String, String);
+
+  private:
+	motorClass *motor;
+	LiquidCrystal *LCD;
+	int trim;
 };
-
-
-
